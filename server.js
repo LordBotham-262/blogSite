8dec6d35070e7b010036ee38cd47fed9d322e048
Dhaval Patel <dhavu262@gmail.com>
2021-08-29 10:48:20 +0530

    initial commit using NextJS

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const app = require("./app");
-
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
-});
